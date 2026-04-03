252696f48faa6d244b4747687e50fd21ddd1a166
aroramit17 <aroramit.17@gmail.com>
2026-04-03 16:29:36 -0500

    Simplify site to astronomy experience

--- site/src/App.tsx
+++ site/src/App.tsx
@@ -2,19 +2,14 @@ import { Route, Routes } from 'react-router-dom';
 import { OperationProvider } from './context/OperationContext';
 import { MainLayout } from './components/layout/MainLayout';
 import { AstronomyResearchPage } from './pages/AstronomyResearchPage';
-import { HomePage } from './pages/HomePage';
-import { PrivacyPolicyPage } from './pages/PrivacyPolicyPage';
-import { TermsPage } from './pages/TermsPage';
 
 function App() {
   return (
     <OperationProvider>
       <Routes>
         <Route element={<MainLayout />}>
-          <Route path="/" element={<HomePage />} />
+          <Route path="/" element={<AstronomyResearchPage />} />
           <Route path="/astronomy-colleges" element={<AstronomyResearchPage />} />
-          <Route path="/privacy-policy" element={<PrivacyPolicyPage />} />
-          <Route path="/terms" element={<TermsPage />} />
         </Route>
       </Routes>
     </OperationProvider>
